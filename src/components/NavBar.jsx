c9fdbc086f0233d2030333a4e9245f730a8063fc
Anoop Yadav <anoopyadavrocky5@gmail.com>
2023-09-28 05:46:54 +0530

    self sound cloud

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
