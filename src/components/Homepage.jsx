a1ab7c5f5ecc0eca810e90404aea68265618e6e9
Anoop Yadav <anoopyadavrocky5@gmail.com>
2024-01-16 21:43:57 +0530

    images, navbar css, content

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     return (
         <>
             <div>
-                <div className='homepage' style={{
+                <div  className='homepage' style={{
                     backgroundImage: `url("src/images/signup.jpg")`,
                     backgroundRepeat: "no-repeat",
                     backgroundSize: "cover",
@@ -16,9 +16,9 @@ const Homepage = () => {
 
                     <nav>
                         <ul>
-                            <li><img className='logo' src="src/images/signup_logo.png" alt="logo" /></li>
-                            <li><li><button>Sign in</button></li>
-                                <li><button>Create Account</button></li>
+                            <li><img className='logoImg' src="src/images/signup_logo.png" alt="logo" /></li>
+                            <li><li><button className='btn'>Sign in</button></li>
+                                <li><button className='btn'>Create Account</button></li>
                                 <li><h3>For Artist</h3></li></li>
                         </ul>
                     </nav>
@@ -30,7 +30,7 @@ const Homepage = () => {
                             </p>
                         </div>
                         <div>
-                            <button>Sign up for free</button>
+                            <button className='btn'>Sign up for free</button>
                         </div>
                     </div>
                 </div>
@@ -39,22 +39,43 @@ const Homepage = () => {
             <div>
                 {/* <input type="text" placeholder='Search for Music,Bands, Artists, Podcassts....' /> <span>or</span><button>Upload your own</button> */}
                 <h2 style={{ color: "black" }}>Hear what’s trending for free in the SoundCloud community</h2>
-            <div className='gridAlbums'>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 1</p><br /><p>Temp Artist 1</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 2</p><br /><p>Temp Artist 2</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 3</p><br /><p>Temp Artist 3</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 4</p><br /><p>Temp Artist 4</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 5</p><br /><p>Temp Artist 5</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 6</p><br /><p>Temp Artist 6</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 7</p><br /><p>Temp Artist 7</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 8</p><br /><p>Temp Artist 8</p></div>
-                <div className="cellAlbum"><img src='src/images/working.png'></img><br/><p>Temp Song Name 9</p><br /><p>Temp Artist 9</p></div>
-            </div>
+                <div className='gridAlbums'>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 1</p><br /><p>Temp Artist 1</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 2</p><br /><p>Temp Artist 2</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 3</p><br /><p>Temp Artist 3</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 4</p><br /><p>Temp Artist 4</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 5</p><br /><p>Temp Artist 5</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 6</p><br /><p>Temp Artist 6</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 7</p><br /><p>Temp Artist 7</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 8</p><br /><p>Temp Artist 8</p></div>
+                    <div className="cellAlbum"><img src='src/images/working.png'></img><br /><p>Temp Song Name 9</p><br /><p>Temp Artist 9</p></div>
+                </div>
             </div>
 
             <div className='annouce'><h2>Thanks for listening. Now join in.</h2>
                 <h3>Save tracks, follow artists and build playlists. All for free.</h3></div>
-            <div><button>Create Account</button></div>
+            <div><button className='btn'>Create Account</button></div>
+            <hr />
+            <div >
+                <ul className='footerLinks'>
+                    <li>About us</li>
+                    <li>Artist</li>
+                    <li>Resources</li>
+                    <li>Blog</li>
+                    <li>Jobs</li>
+                    <li>Developers</li>
+                    <li>Help</li>
+                    <li>Legal</li>
+                    <li>Privacy</li>
+                    <li>Cookie</li>
+                    <li>Policy</li>
+                    <li>Consent</li>
+                    <li>Manager</li>
+                    <li>Imprint</li>
+                    <li>Charts</li>
+                    <li><h3>Language: English (US)</h3></li>
+                </ul>
+            </div>
         </>
     )
 }
