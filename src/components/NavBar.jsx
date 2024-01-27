19c7954d2578a72b51f22546b976c9989a9feb6c
Anoop Yadav <anoopyadavrocky5@gmail.com>
2024-01-27 21:40:59 +0530

    style:modal para alignment homepage new class para

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,12 @@ const NavBar = () => {
     return (
         <nav className='parentNavBar'>
             <ul className='navBar'>
-                <li><Link to="/dashboard"><img src="/src/images/logo.png" alt="" /></Link></li>
-                <li><Link to="/dashboard">Home</Link></li>
-                <li><Link to="/feed">Feed</Link></li>
-                <li><Link to="/library">Library</Link></li>
-                <li><input className='searchBar' type="search" placeholder='Search' /></li>
-                <li><Link to="/userInfo"><h3>Anoop</h3> </Link></li>
+                <li className='navBarItem'><Link to="/dashboard"><img src="/src/images/logo.png" alt="" /></Link></li>
+                <li className='navBarItem'><Link to="/dashboard">Home</Link></li>
+                <li className='navBarItem'><Link to="/feed">Feed</Link></li>
+                <li className='navBarItem'><Link to="/library">Library</Link></li>
+                <li className='navBarItem'><input className='searchBar' type="search" placeholder='Search' /></li>
+                <li className='navBarItem'><Link to="/userInfo"><h3>Anoop</h3> </Link></li>
             </ul>
         </nav>
     );
--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Sidebar from '../components/Sidebar'
+import AlbumGrid from '../components/AlbumGrid'
 
 const Dashboard = () => {
   return (
@@ -8,7 +9,9 @@ const Dashboard = () => {
       <div><NavBar /></div>
      
       <div className='homeDivs'>
-        <div className='songsDiv'>content different music with filters</div>
+        <div className='songsDiv'>
+          <AlbumGrid />
+          content different music with filters</div>
          <hr />
         <div><Sidebar /></div>
       </div>
