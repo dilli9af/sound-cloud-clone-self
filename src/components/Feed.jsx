6a21c193f1b97ac7f1e0f847d640b81f00a5e8db
Anoop Yadav <anoopyadavrocky5@gmail.com>
2024-01-27 21:40:44 +0530

    fix: Routing Bahavior Link react-router-dom

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import NavBar from './NavBar';
 import Sidebar from './Sidebar';
+import AlbumGrid from './AlbumGrid';
 
 const Feed = () => {
   return (
     <>
       <NavBar />
       <div className='feedDivs'>
-        <div> <h3>Apeended songs list infinite scroll</h3></div>
+      
+        {/* <div> <h3>Apeended songs list infinite scroll</h3></div> */}
+        <div className='feedItem'><AlbumGrid /></div>
         <hr />
-        <div><Sidebar /></div>
+        <div className='feedItem'><Sidebar /></div>
+        
+        
+        
       </div>
 
     </>
