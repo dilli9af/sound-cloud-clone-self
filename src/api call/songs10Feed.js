5f9c277a1ba3ae9bd21fd5657ac440d6104c72a1
Anoop Yadav <anoopyadavrocky5@gmail.com>
2024-01-30 21:52:57 +0530

    test:song object data

---
+++ src/api call/songs10Feed.js
@@ -0,0 +1,376 @@
+var songData={
+    "data": [
+        {
+            "_id": "64cf907d47ae38c3e33a189a",
+            "title": "Kohinoor",
+            "artist": [
+                {
+                    "_id": "64ce7c0bbbbada037c35eda9",
+                    "name": "DIVINE",
+                    "description": "Emotive storyteller, infusing lyrics with heartfelt meaning. Using music to inspire positive change, a beacon of hope.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/148de808-8598-4066-8fe0-972e78d834d8size_xl_1663590345.webp",
+                    "languages": [
+                        "punjabi",
+                        "tamil",
+                        "hindi",
+                        "bengali",
+                        "telugu",
+                        "malayalam",
+                        "english",
+                        "marathi",
+                        "bhojpuri",
+                        "gujarati"
+                    ],
+                    "songs": [
+                        "64cf907d47ae38c3e33a189a",
+                        "64cf909147ae38c3e33a1a3a",
+                        "64cf909e47ae38c3e33a1b4b",
+                        "64cf920b47ae38c3e33a3889",
+                        "64cf949647ae38c3e33a6c29",
+                        "64cf94cf47ae38c3e33a70a6",
+                        "64cf94cf47ae38c3e33a70ae",
+                        "64cf94d047ae38c3e33a70b8",
+                        "64cf94d047ae38c3e33a70c2",
+                        "64cf94d147ae38c3e33a70cc",
+                        "64cf94d147ae38c3e33a70d5",
+                        "64cf94d247ae38c3e33a70dc",
+                        "64cf94d247ae38c3e33a70e4",
+                        "64cf94d347ae38c3e33a70ed",
+                        "64cf94d347ae38c3e33a70f4",
+                        "64cf94d347ae38c3e33a70fb",
+                        "64cf94d447ae38c3e33a7103"
+                    ],
+                    "appType": "music",
+                    "__v": 17
+                }
+            ],
+            "likedBy": [],
+            "type": "song",
+            "mood": "happy",
+            "thumbnail": "https://newton-project-resume-backend.s3.amazonaws.com/thumbnail/64cf907d47ae38c3e33a189a.jpg",
+            "appType": "music",
+            "createdAt": "2023-08-06T12:22:21.474Z",
+            "__v": 0,
+            "audio_url": "https://newton-project-resume-backend.s3.amazonaws.com/audio/64cf907d47ae38c3e33a189a.mp3",
+            "featured": "Trending songs"
+        },
+        {
+            "featured": null,
+            "_id": "64cf907d47ae38c3e33a18a2",
+            "title": "Chak De India",
+            "artist": [
+                {
+                    "_id": "64ce7c0bbbbada037c35edaa",
+                    "name": "Sukhwinder Singh",
+                    "description": "Electrifying stage presence, leaving fans in awe of their magnetic aura. A trailblazer embracing music's evolution.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/cc955507-6197-4eb1-b87c-0e9ecddb913dsize_xl_1516795350.webp",
+                    "languages": [
+                        "punjabi",
+                        "tamil",
+                        "hindi",
+                        "bengali",
+                        "telugu",
+                        "malayalam",
+                        "kannada",
+                        "english",
+                        "marathi",
+                        "gujarati"
+                    ],
+                    "songs": [
+                        "64cf907d47ae38c3e33a18a2",
+                        "64cf90a047ae38c3e33a1b74",
+                        "64cf90be47ae38c3e33a1dc4",
+                        "64cf90c747ae38c3e33a1e79",
+                        "64cf90c947ae38c3e33a1e9f",
+                        "64cf912c47ae38c3e33a269d",
+                        "64cf912c47ae38c3e33a26ac",
+                        "64cf91e047ae38c3e33a3529",
+                        "64cf91e247ae38c3e33a3546",
+                        "64cf91fe47ae38c3e33a376f",
+                        "64cf91fe47ae38c3e33a377d",
+                        "64cf91ff47ae38c3e33a378c",
+                        "64cf92b647ae38c3e33a454a",
+                        "64cf92ce47ae38c3e33a4736",
+                        "64cf943147ae38c3e33a6414",
+                        "64cf946d47ae38c3e33a68e1",
+                        "64cf947547ae38c3e33a6986",
+                        "64cf94eb47ae38c3e33a72e6"
+                    ],
+                    "appType": "music",
+                    "__v": 18
+                },
+                {
+                    "_id": "64ce7c0bbbbada037c35edab",
+                    "name": "Salim Merchant",
+                    "description": "A vocal virtuoso, effortlessly hitting dazzling high notes. A star whose presence illuminates the darkest stages.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/dbae5060-20fa-4f2f-9db6-314ed109f80bsize_xl_1558421125.webp",
+                    "languages": [
+                        "punjabi",
+                        "tamil",
+                        "urdu",
+                        "hindi",
+                        "bengali",
+                        "malayalam",
+                        "english",
+                        "marathi",
+                        "gujarati"
+                    ],
+                    "songs": [
+                        "64cf907d47ae38c3e33a18a2",
+                        "64cf907e47ae38c3e33a18ae",
+                        "64cf90be47ae38c3e33a1dc4",
+                        "64cf912c47ae38c3e33a26ac",
+                        "64cf920947ae38c3e33a3864",
+                        "64cf930e47ae38c3e33a4c7b",
+                        "64cf930f47ae38c3e33a4c8c",
+                        "64cf943147ae38c3e33a6414"
+                    ],
+                    "appType": "music",
+                    "__v": 8
+                },
+                {
+                    "_id": "64ce7c0bbbbada037c35edac",
+                    "name": "Marianne D-Cruz",
+                    "description": "A soulful crooner with a captivating voice, evoking deep emotions with each note. Enchanting audiences worldwide.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/b69d84a9-8340-4287-af4f-9c95759b47eesize_xl.jpg",
+                    "languages": [
+                        "tamil",
+                        "hindi",
+                        "english"
+                    ],
+                    "songs": [
+                        "64cf907d47ae38c3e33a18a2",
+                        "64cf90be47ae38c3e33a1dc4",
+                        "64cf912c47ae38c3e33a26ac",
+                        "64cf931247ae38c3e33a4cc5",
+                        "64cf943147ae38c3e33a6414"
+                    ],
+                    "appType": "music",
+                    "__v": 5
+                }
+            ],
+            "album": "64cee72fe41f6d0a8b0cd0a8",
+            "likedBy": [],
+            "dateOfRelease": "2007-08-01T00:00:00.000Z",
+            "type": "song",
+            "mood": "romantic",
+            "thumbnail": "https://newton-project-resume-backend.s3.amazonaws.com/thumbnail/64cf907d47ae38c3e33a18a2.jpg",
+            "appType": "music",
+            "createdAt": "2023-08-06T12:22:21.894Z",
+            "__v": 0,
+            "audio_url": "https://newton-project-resume-backend.s3.amazonaws.com/audio/64cf907d47ae38c3e33a18a2.mp3"
+        },
+        {
+            "featured": null,
+            "_id": "64cf907e47ae38c3e33a18ae",
+            "title": "Maula Mere Lele Meri Jaan",
+            "artist": [
+                {
+                    "_id": "64ce7c0bbbbada037c35edb2",
+                    "name": "Krishna",
+                    "description": "Charismatic performer, owning the stage with a magnetic presence. An innovative artist pushing creative boundaries fearlessly.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/3e8ada89-81b4-43c6-bc40-664d4457eaffsize_xl.jpg",
+                    "languages": [
+                        "punjabi",
+                        "tamil",
+                        "urdu",
+                        "hindi",
+                        "bengali",
+                        "telugu",
+                        "malayalam",
+                        "kannada",
+                        "english",
+                        "marathi",
+                        "assamese",
+                        "odia",
+                        "bhojpuri",
+                        "gujarati",
+                        "rajasthani"
+                    ],
+                    "songs": [
+                        "64cf907e47ae38c3e33a18ae",
+                        "64cf946147ae38c3e33a67e6",
+                        "64cf946447ae38c3e33a6837",
+                        "64cf946647ae38c3e33a6855"
+                    ],
+                    "appType": "music",
+                    "__v": 4
+                },
+                {
+                    "_id": "64ce7c0bbbbada037c35edab",
+                    "name": "Salim Merchant",
+                    "description": "A vocal virtuoso, effortlessly hitting dazzling high notes. A star whose presence illuminates the darkest stages.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/dbae5060-20fa-4f2f-9db6-314ed109f80bsize_xl_1558421125.webp",
+                    "languages": [
+                        "punjabi",
+                        "tamil",
+                        "urdu",
+                        "hindi",
+                        "bengali",
+                        "malayalam",
+                        "english",
+                        "marathi",
+                        "gujarati"
+                    ],
+                    "songs": [
+                        "64cf907d47ae38c3e33a18a2",
+                        "64cf907e47ae38c3e33a18ae",
+                        "64cf90be47ae38c3e33a1dc4",
+                        "64cf912c47ae38c3e33a26ac",
+                        "64cf920947ae38c3e33a3864",
+                        "64cf930e47ae38c3e33a4c7b",
+                        "64cf930f47ae38c3e33a4c8c",
+                        "64cf943147ae38c3e33a6414"
+                    ],
+                    "appType": "music",
+                    "__v": 8
+                }
+            ],
+            "album": "64cee72fe41f6d0a8b0cd0a8",
+            "likedBy": [],
+            "dateOfRelease": "2007-08-01T00:00:00.000Z",
+            "type": "song",
+            "mood": "excited",
+            "thumbnail": "https://newton-project-resume-backend.s3.amazonaws.com/thumbnail/64cf907e47ae38c3e33a18ae.jpg",
+            "appType": "music",
+            "createdAt": "2023-08-06T12:22:22.493Z",
+            "__v": 0,
+            "audio_url": "https://newton-project-resume-backend.s3.amazonaws.com/audio/64cf907e47ae38c3e33a18ae.mp3"
+        },
+        {
+            "featured": null,
+            "_id": "64cf907e47ae38c3e33a18b7",
+            "title": "Badal Pe Paon Hai",
+            "artist": [
+                {
+                    "_id": "64ce7c0bbbbada037c35edad",
+                    "name": "Hema Sardesai",
+                    "description": "Rhythmic charisma, infusing each performance with infectious energy. A trendsetting artist with global appeal.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/ee685600-3957-4599-9a32-12990619cc4esize_xl.jpg",
+                    "languages": [
+                        "tamil",
+                        "hindi",
+                        "bengali",
+                        "telugu",
+                        "marathi",
+                        "assamese"
+                    ],
+                    "songs": [
+                        "64cf907e47ae38c3e33a18b7",
+                        "64cf913047ae38c3e33a26fd"
+                    ],
+                    "appType": "music",
+                    "__v": 2
+                }
+            ],
+            "album": "64cee72fe41f6d0a8b0cd0a8",
+            "likedBy": [],
+            "dateOfRelease": "2007-08-01T00:00:00.000Z",
+            "type": "song",
+            "mood": "sad",
+            "thumbnail": "https://newton-project-resume-backend.s3.amazonaws.com/thumbnail/64cf907e47ae38c3e33a18b7.jpg",
+            "appType": "music",
+            "createdAt": "2023-08-06T12:22:22.924Z",
+            "__v": 0,
+            "audio_url": "https://newton-project-resume-backend.s3.amazonaws.com/audio/64cf907e47ae38c3e33a18b7.mp3"
+        },
+        {
+            "featured": null,
+            "_id": "64cf907f47ae38c3e33a18c0",
+            "title": "Ek Hockey Doongi Rakh Ke",
+            "artist": [
+                {
+                    "_id": "64ce7c0bbbbada037c35edae",
+                    "name": "Shahrukh Khan",
+                    "description": "Electrifying stage presence, leaving fans in awe of their magnetic aura. A trailblazer embracing music's evolution.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/631b3429-c8e7-4230-90df-048dfde6e74asize_xl_1541149171.webp",
+                    "languages": [
+                        "punjabi",
+                        "hindi",
+                        "bengali",
+                        "english",
+                        "bhojpuri",
+                        "haryanvi"
+                    ],
+                    "songs": [
+                        "64cf907f47ae38c3e33a18c0",
+                        "64cf908047ae38c3e33a18d8",
+                        "64cf943147ae38c3e33a6420"
+                    ],
+                    "appType": "music",
+                    "__v": 3
+                },
+                {
+                    "_id": "64ce7c0bbbbada037c35edaf",
+                    "name": "K.K.",
+                    "description": "Charismatic performer, owning the stage with a magnetic presence. An innovative artist pushing creative boundaries fearlessly.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/de025173-a1f2-4ad5-9877-b315ede3d6e3size_xl_1558442140.webp",
+                    "languages": [
+                        "punjabi",
+                        "tamil",
+                        "hindi",
+                        "bengali",
+                        "telugu",
+                        "malayalam",
+                        "kannada",
+                        "english",
+                        "marathi",
+                        "assamese",
+                        "bhojpuri",
+                        "gujarati",
+                        "rajasthani"
+                    ],
+                    "songs": [
+                        "64cf907f47ae38c3e33a18c0",
+                        "64cf90a647ae38c3e33a1be8",
+                        "64cf910e47ae38c3e33a2434",
+                        "64cf912e47ae38c3e33a26d5",
+                        "64cf916d47ae38c3e33a2bcc",
+                        "64cf916e47ae38c3e33a2be2",
+                        "64cf916e47ae38c3e33a2bec",
+                        "64cf91ac47ae38c3e33a30ff",
+                        "64cf91ad47ae38c3e33a3109",
+                        "64cf923447ae38c3e33a3bc9",
+                        "64cf923647ae38c3e33a3bfe",
+                        "64cf924647ae38c3e33a3d3a",
+                        "64cf92b547ae38c3e33a453a",
+                        "64cf92b647ae38c3e33a454a",
+                        "64cf934647ae38c3e33a5108",
+                        "64cf934647ae38c3e33a5112",
+                        "64cf934847ae38c3e33a513a",
+                        "64cf938147ae38c3e33a55ca",
+                        "64cf938147ae38c3e33a55d6",
+                        "64cf942347ae38c3e33a62f1",
+                        "64cf942347ae38c3e33a62f8",
+                        "64cf942347ae38c3e33a62ff",
+                        "64cf942447ae38c3e33a6306",
+                        "64cf942447ae38c3e33a630d",
+                        "64cf942447ae38c3e33a6314",
+                        "64cf942547ae38c3e33a631b",
+                        "64cf942547ae38c3e33a6322",
+                        "64cf943547ae38c3e33a6468",
+                        "64cf943647ae38c3e33a6488",
+                        "64cf943847ae38c3e33a649f",
+                        "64cf943847ae38c3e33a64ac",
+                        "64cf94df47ae38c3e33a71f3"
+                    ],
+                    "appType": "music",
+                    "__v": 32
+                },
+                {
+                    "_id": "64ce7c0bbbbada037c35edb0",
+                    "name": "The Hockey team",
+                    "description": "Charismatic performer, owning the stage with a magnetic presence. An innovative artist pushing creative boundaries fearlessly.",
+                    "image": "https://newton-project-resume-backend.s3.ap-south-1.amazonaws.com/52a2a840-0628-439d-bf93-ed8681b8417esize_xl.jpg",
+                    "languages": [
+                        "hindi"
+                    ],
+                    "songs": [
+                        "64cf907f47ae38c3e33a18c0"
+                    ],
+                    "appType": "music",
+                    "__v": 1
+                }
+            ],   }
+    ]
+}
