bc67a18fb5a01aeb1120d07cd73a8328c3dd9779
Anoop Yadav <anoopyadavrocky5@gmail.com>
2024-02-01 07:35:17 +0530

    fix:audio player functionaty with onclick event

--- src/components/SongFeed.jsx
+++ src/components/SongFeed.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-
+import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
 const SongFeed = () => {
   const [songData, setSongData] = useState({ data: [] });
   const [currentSongIndex, setCurrentSongIndex] = useState(null);
+
 
   useEffect(() => {
     async function fetchData() {
@@ -39,28 +40,42 @@ const SongFeed = () => {
       setCurrentSongIndex(index);
     } else {
       audio.pause();
-      setCurrentSongIndex(null);
+      // setCurrentSongIndex(null);
     }
   };
 
+  const formatMood = (mood) => {
+    return mood.charAt(0).toUpperCase() + mood.slice(1).toLowerCase();
+  };
   const currentSong = currentSongIndex !== null ? songData.data[currentSongIndex] : null;
 
   return (
     <div>
       {songData.data.map((song, index) => (
-        <div key={index} className="container" onClick={() => handlePlayPause(index)}>
-          <img src={song.thumbnail} alt="thumbnail" />
-          <p>{song.title}</p>
-          <h3>{song.artist[0].name}</h3>
-          <audio id={`mainAudio-${index}`} src={song.audio_url}></audio>
+        <div key={index} className="container" >
+          <div><img src={song.thumbnail} alt="thumbnail" onClick={() => handlePlayPause(index)} />
+            <audio id={`mainAudio-${index}`} src={song.audio_url}></audio>
+          </div>
+          <div>
+            <p onClick={() => handlePlayPause(index)}>{song.title}</p>
+            <h4 onClick={() => handlePlayPause(index)}>{song.artist[0].name}</h4>
+            <p onClick={() => handlePlayPause(index)}>{formatMood(song.mood)}</p>
+            <FavoriteBorderSharpIcon />
+          </div>
+
+
         </div>
       ))}
       <div >
         {currentSong && (
           <div className="footerSongs">
-            <div><img src={currentSong.thumbnail} alt="current thumbnail" /></div>
-            <div><p>{currentSong.title}</p>
-            <h3>{currentSong.artist[0].name}</h3></div>
+            <div>
+              <img src={currentSong.thumbnail} alt="current thumbnail" />
+            </div>
+            <div>
+              <p>{currentSong.title}</p>
+              <h3>{currentSong.artist[0].name}</h3>
+            </div>
 
           </div>
         )}
